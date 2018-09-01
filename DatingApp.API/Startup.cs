619eccf52e8c07c8e50bcc82546617b1fe8d2950
shaun-rostami <shaun.rostami@just-eat.com>
2018-09-01 17:59:31 +1000

    wwwroot and pageSize

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -93,7 +93,14 @@ namespace DatingApp.API
             //seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
